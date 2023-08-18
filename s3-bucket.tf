a28e162a3bcdf41a2030daab32dee56676282738
Vicky Jacob <vwj@amazon.com>
2023-08-18 19:24:18 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,14 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  bucket_prefix = "Vicky"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
